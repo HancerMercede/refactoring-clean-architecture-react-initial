68ce006b606514d1c8b9cfea5802e349bf19be13
Hancer <hancermercedes@gmail.com>
2024-06-01 21:11:55 -0400

    add msw and create a wrapper or adapter to manage the logic

--- src/pages/test/ProductsPage.spec.tsx
+++ src/pages/test/ProductsPage.spec.tsx
@@ -1,14 +1,37 @@
-import { test } from "vitest";
+import { afterAll, beforeAll, describe, test } from "vitest";
 import { RenderResult, render, screen } from "@testing-library/react";
 import { ProductsPage } from "../ProductsPage";
 import { AppProvider } from "../../context/AppProvider";
 import { ReactNode } from "react";
+import { MockWebServer } from "../../tests/MockWebServer";
+import productResponse from "./data/productsResponse.json";
 
-test("loads and displays title", () => {
-    renderComponent(<ProductsPage />);
+const mockWebServer = new MockWebServer();
 
-    screen.findAllByRole("heading", { name: "Product price updater" });
+describe("Products Page", () => {
+    beforeAll(() => mockWebServer.start());
+    afterAll(() => mockWebServer.resetHandlers());
+    afterAll(() => mockWebServer.close());
+
+    test("loads and displays title", () => {
+        givenAProducts();
+
+        renderComponent(<ProductsPage />);
+
+        screen.findAllByRole("heading", { name: "Product price updater" });
+    });
 });
+
+function givenAProducts() {
+    mockWebServer.addRequestHandlers([
+        {
+            method: "get",
+            endpoint: "https://fakestoreapi.com/products",
+            httpStatusCode: 200,
+            response: productResponse,
+        },
+    ]);
+}
 
 function renderComponent(component: ReactNode): RenderResult {
     return render(<AppProvider>{component}</AppProvider>);
